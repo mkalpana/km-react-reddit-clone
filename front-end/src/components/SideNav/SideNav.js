acb3699999e0214436c1d4bf5cd5029fc9a21d8f
Kalpana Madhalam <mkalpanais@gmail.com>
2017-10-10 18:30:47 +1100

    Added SideNav component

--- front-end/src/components/SideNav/SideNav.js
+++ front-end/src/components/SideNav/SideNav.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import './SideNav.css';
 
 class SideNav extends Component{
   render() {
+    const { links } = this.props;
     return (
-      <div>SideNav</div>
+      <div className="SideNav-container">
+        <h2>Categories</h2>
+        {
+          links && links.map(link => {
+            return (
+              <div>
+                <Link key={link.name} to={link.url}>{link.name}</Link>
+              </div>
+            )
+          })
+        }
+      </div>
     );
   }
 }
 SideNav.propTypes = {
-
-}
+  links: PropTypes.array,
+};
 export default SideNav;
